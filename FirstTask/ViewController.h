24167d8192c350121f03d3b8649ffdf2841457d3
Oleg WorkPC <omyatlikov@intervale.ru>
2017-04-21 09:13:37 +0300

    Initial Commit

---
+++ FirstTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstTask
+//
+//  Created by Admin on 21.04.17.
+//  Copyright © 2017 Oleg Myatlikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstTask
+//
+//  Created by Admin on 21.04.17.
+//  Copyright © 2017 Oleg Myatlikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
