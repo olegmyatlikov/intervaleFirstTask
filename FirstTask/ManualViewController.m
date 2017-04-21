02646f01637299306addfb0fbb71412fa69d32bc
Oleg WorkPC <omyatlikov@intervale.ru>
2017-04-21 18:10:27 +0300

    Create manual UI

--- FirstTask/ManualViewController.m
+++ FirstTask/ManualViewController.m
@@ -16,34 +16,43 @@
     //UIView *view = [[UIView alloc] initWithFrame:self.view.bounds];
     //view.backgroundColor = [UIColor blueColor];
     
-    // first name
+    // first name LABEL
     UILabel *firstNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, 150, 100, 30)];
+    [firstNameLabel setFont:[UIFont systemFontOfSize:15]];
     [firstNameLabel setTextColor: [UIColor blackColor]];
     [firstNameLabel setText:@"First name"];
     [self.view addSubview:firstNameLabel];
     
+    // fist name TEXTFIELD
     UITextField *firstNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(140, 150, 140, 30)];
+    [firstNameTextField setFont:[UIFont systemFontOfSize:15]];
     [firstNameTextField borderStyle];
     [firstNameTextField setTextAlignment: NSTextAlignmentCenter];
     [firstNameTextField setBorderStyle:UITextBorderStyleRoundedRect];
     [self.view addSubview:firstNameTextField];
     
-    //last name
+     // last name LABEL
     UILabel *lastNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, 200, 100, 30)];
+    [lastNameLabel setFont:[UIFont systemFontOfSize:15]];
     [lastNameLabel setTextColor: [UIColor blackColor]];
     [lastNameLabel setText:@"Last name"];
     [self.view addSubview:lastNameLabel];
     
+    // last name TEXTFIELD
     UITextField *lastNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(140, 200, 140, 30)];
+    [lastNameTextField setFont:[UIFont systemFontOfSize:15]];
     [lastNameTextField borderStyle];
     [lastNameTextField setTextAlignment: NSTextAlignmentCenter];
     [lastNameTextField setBorderStyle:UITextBorderStyleRoundedRect];
     [self.view addSubview:lastNameTextField];
 
-    
-    UIButton *buttomSayHello = [[UIButton alloc] initWithFrame:CGRectMake(100, 20, 200, 50)];
-    buttomSayHello.backgroundColor = [UIColor blueColor];
-    
+    // say hello BUTTOM
+    UIButton *buttomSayHello = [[UIButton alloc] initWithFrame:CGRectMake(40, 260, 240, 30)];
+    [buttomSayHello setTintColor:[UIColor whiteColor]];
+    [buttomSayHello.titleLabel setFont:[UIFont systemFontOfSize:15]];
+    [buttomSayHello setBackgroundColor: [UIColor colorWithRed:0 green:122 blue:255 alpha:1]];
+    [buttomSayHello setTitle:@"Say hello" forState:UIControlStateNormal];
+    [self.view addSubview:buttomSayHello];
     
 }
 
