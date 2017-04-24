13d5ee870ff8d7ca1a0f4adb779da33f6d43c43c
olegmyatlikov <olegmyatlikov@tut.by>
2017-04-24 12:44:22 +0300

    Add getter/setter for property and some methods

--- FirstTask/User.m
+++ FirstTask/User.m
@@ -8,6 +8,11 @@
 
 #import "User.h"
 
+@interface User() {
+    NSMutableArray *_followers;
+    NSMutableArray *_following;  
+}
+
 @implementation User
 @synthesize userId = _userId;
 @synthesize firstName = _firstName;
@@ -19,6 +24,9 @@
 @synthesize city = _city;
 @synthesize country = _country;
 @synthesize contactUser = _contactUser;
+
+
+// Инициализаторы
 
 -(id) initUserWithID: (NSNumber*) userId
            firstName: (NSMutableString*) name
@@ -45,31 +53,116 @@
     return self;
 }
 
--(NSNumber*) userId {
-    NSLog(@"getter userId called");
-    return _userId;
+
+// get&set userId (atomic, retain)
+
+-(NSNumber *) userId {
+    NSLog(@"getter userId was called");
+    return [[userId retain] autorelease];
 }
 
--(void) setUserId:(NSNumber *)userId {
-    NSLog(@"setter userId called");
-    _userId = userId;
+-(void) setUserId:(NSNumber *) userId {
+    NSLog(@"setter userId was called");
+    [_userId release];
+    [userId retain];
+    _userId = userId;       
 }
 
+
+// get&set firstName (nonatomic, retain)
+
+-(NSString *) firstName {
+   NSLog(@"getter firstName was called");
+   return [[firstName retain] autorelease];                   
+}           
+
+-(void) setFirstName:(NSString *) firstName {
+    NSLog(@"setter firstName was called");
+    [_firstName release];
+    [firstName retain];
+    _firstName = firstName;       
+}
+
+
+// get&set lastName (nonatomic, copy)
+
+-(NSString *) lastName {
+   NSLog(@"getter lastName was called");
+   return [[lastName copy] autorelease];                   
+}           
+
+-(void) setLastName:(NSString *) lastName {
+    NSLog(@"setter lastName was called");
+    if (_lastName != lastName) {
+       [_lastName release];
+       _lastName = [lastName copy];        
+    }
+}
+
+
+// get&set folowers (atomic, copy)
+
+-(NSArray *) folowers {
+   NSLog(@"getter folowers was called");
+   return [[folowers copy] autorelease];                   
+}           
+-(void) folowers:(NSArray *) folowers { 
+   if (folowers != folowers) {
+   [_folowers release];
+   _folowers = [folowers copy];        
+   }
+}
+
+
+// get&set folowers (nonatomic, assign)
+
+-(BOOL) contactUser {
+    NSLog(@"getter contactUser was called");
+    return contactUser;//???
+}
+
+-(void) setContactUser:(NSNumber *) contactUser {
+    NSLog(@"setter contactUser was called");
+    [contactUser release];       
+}
+
+
+-(void) printFullName {
+    NSLog[@"%@ %@.", self.firstName, self.lastName];           
+}
+
+-(void) addFolower: (id) user {
+    [_folower addObject: user];           
+}
+
+-(void) removeFolower: (id) user {
+    [_folower removeObject: user];           
+}
+
+-(void) addFolowing: (id) user {
+    [_folowing addObject: user];           
+}
+
+-(void) removeFolowing: (id) user {
+    [_folowing removeObject: user];           
+}
+
+-(BOOL) isFollowerPerson: (id) user {
+    return [_folower containsObject: user]; 
+}
+
+-(BOOL) isFollowingPerson: (id) user {
+    return [_folowing containsObject: user]; 
+}
+
+-(void) description: (id) user {
+    NSLog[@" User ID: %@ \n Name: %@ \n Lastname: %@ \n Birthday: %@ \n Folowers: %@ \n Folowing: %@ \n City: %@ \n Country: %@", user.UserID, user.firstName, user.lastName, user.folowers, user.folowing, user.city, user.country];          
+}
 
 -(void) dealloc {
     NSLog(@"dealloc %@", self);
 
-    [_userId release];
-    [_firstName release];
-    [_lastName release];
     [_birthDate release];
-    [_folowers release];
-    [_folowing release];
-    [_address release];
-    [_city release];
-    [_country release];
-    [_country release];
-    
     [super dealloc];
 }
 
